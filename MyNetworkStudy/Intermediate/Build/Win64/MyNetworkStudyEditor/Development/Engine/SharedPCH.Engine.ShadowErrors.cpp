a10958fb8f2e0a87738b98f8b923dee26f94d9fb
ImTaeYeonLee <90662629+ImTaeYeonLee@users.noreply.github.com>
2021-10-24 23:45:36 +0900

    MyNetworkStudy->오디오 네트워크, 채팅창 작업중

--- MyNetworkStudy/Intermediate/Build/Win64/MyNetworkStudyEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30133
-#include "C:/Users/TFG5076XG/Desktop/MyNetworkStudy/Intermediate/Build/Win64/MyNetworkStudyEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ MyNetworkStudy/Source/MyNetworkStudy/Private/UW_Main.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UW_Main.h"
+#include "UW_Chat.h"
+
+//TSharedPtr<SWidget> UUW_Main::GetChatInputTextObject()
+//{
+//	return WB_Chat->GetChatInputTextObject();
+//}
+
+//void UUW_Main::AddChatMessage(const FString& Message)
+//{
+//	WB_Chat->AddChatMessage(Message);
+//}
